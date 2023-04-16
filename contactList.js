88b8b534429ff2d6c22f05fe1acdbc651f7d4fcb
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-04-16 04:13:40 -0500

    Se desarrolla la práctica de JS de Lista de contactos - 1

---
+++ contactList.js
@@ -0,0 +1,53 @@
+
+// Crear lista de contactos con datos predifinidos
+// Cada contacto contiene el nombre y apellido en un solo string
+
+let misContactos = [
+    {nombreApellido: "Maria Alejandra Amaya Rodriguez"},
+    {nombreApellido: "Vanessa Mejía"},
+    {nombreApellido: "Luz Dary Zapata Valencia"},
+    {nombreApellido: "Walter Arroyave"},
+    {nombreApellido: "Andrés Felipe Arroyave Zapata"},
+    {nombreApellido: "Leticia Rodriguez"}
+];
+
+console.log(misContactos); 
+
+/// Función para añadir un nuevo contacto ///
+
+// const anadirContacto = function(misContactos, nombreApellido){
+//     misContactos.push({nombreApellido});
+// }
+
+// anadirContacto(misContactos, "Lucas Antonio Arroyave");
+
+function anadirContacto(misContactos, nombreApellido){
+    misContactos.push({nombreApellido});
+}
+
+anadirContacto(misContactos, "Lucas Antonio Arroyave");
+
+console.log(misContactos);
+
+/// Funcion para borra un contacto de la lista ///
+
+const eliminarContacto = function(misContactos, nombreApellido){
+    for (let i = 0; misContactos.length; i++){
+        if (misContactos[i].nombreApellido===nombreApellido){
+            misContactos.splice(i, 1);
+            break;
+        }
+    }
+}
+
+eliminarContacto(misContactos, "Leticia Rodriguez"); // Voy a eliminar el contacto Leticia Rodriguez de mi lista
+
+console.log(misContactos);
+
+/// Crea una función para imprimir en consola los contactos presentes en la lista ///
+
+function verContactos(){
+    console.log(misContactos);
+}
+verContactos();
+
